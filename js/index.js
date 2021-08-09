58164a91228e95b24f9e74c2144265239978729b
Ana C <apspfc@gmail.com>
2021-08-09 15:45:47 +0100

    1st ite

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+const hacker1 = "";
+console.log(`The drivers name is ${hacker1}.`);
+const hacker2 = "";
+console.log(`The navigator´s name is ${}.`)
 
 // Iteration 2: Conditionals
 
